36c45bf45382e348eb81ce71424c78ae5ef58c8f
David L. Trueba <18346@columbushs.com>
2017-12-21 12:48:34 -0500

    my first commit

---
+++ src/notepad/notepad.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package notepad;
+
+/**
+ *
+ * @author 18346
+ */
+
+
+import java.util.Scanner;
+public class notepad {
+//private final int FIRST;
+//private int second;
+//public notepad(int first, int secNum){
+//    FIRST = first;
+//    second = secNum;
+//}
+//public String getProblem(){
+//    return FIRST + " TIMES " + second;
+//}
+//public void nextProblem(){
+//    second++;
+//}
+
+public static Scanner in = new Scanner(System.in);
+public static void main(String[] args) {
+  String statement = "hili"
+          + "";
+  int num = statement.indexOf("i");
+    System.out.println(num);
+}}
+    
+
+
+    
+    
+    
+   
+
+ 
+
+
